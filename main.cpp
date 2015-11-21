023ccdbcc720c9f5f117d60c9308f4e7b7dec7fe
wgrabis <wgrabis@gmail.com>
2015-11-21 22:20:16 +0100

    JNI prototype

--- main.cpp
+++ main.cpp
@@ -1,10 +1,17 @@
 #include <iostream>
 #include <boost/array.hpp>
+#include <jni.h>
+#include "JNI-test/SimpleInstanceJNI.h"
 
 using namespace std;
 
 int main(){
-    boost::array<int, 4> arr = {{1,2,3,4}};
-    cout << "hi" << arr[0];
+
+
+    //boost::array<int, 4> arr = {{1,2,3,4}};
+    //cout << "hi" << arr[0];
+    SimpleInstanceJNI instanceJNI;
+    instanceJNI.runSimpleWriter("pathToFile");
+    instanceJNI.close();
     return 0;
 }
