2a47120f0c41fa2b5ec94c385c877afe48d2fa86
wiktorj <Wiktor.Jurasz@gmail.com>
2015-12-11 00:20:57 +0100

    added basic obcerver

---
+++ ControllersContainer/src/main/java/to2/ds/game/controllers/Observer.java
@@ -0,0 +1,19 @@
+package to2.ds.game.controllers;
+
+import org.json.JSONObject;
+
+/**
+ * Created by marcinsendera on 08.12.2015.
+ */
+//import java.util.Observable;
+//import java.util.Observer;
+
+public interface Observer {
+
+    //private String state;
+
+    public void stateUpdated(JSONObject state) throws InterruptedException;   //docelowo stateUpdated(GameState state)
+    public void gameEnded() throws InterruptedException;
+    public void removePlayer(String nickname) throws InterruptedException;
+
+}
